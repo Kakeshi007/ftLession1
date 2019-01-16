6eba818b80854c90db4eeb05e483aeeec7ef41d1
Kakeshi007 <kakeshi007@gmail.com>
2019-01-16 14:38:39 +0700

    delete console

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -28,7 +28,6 @@ class _LoginPageState extends State<LoginPage> {
   Future<Null> _handleGoogleSignIn() async {
     try {
       GoogleSignInAccount googleSignInAccount = await _googleSignIn.signIn();
-      print('add git');
     } catch (error) {
       print(error);
     }
